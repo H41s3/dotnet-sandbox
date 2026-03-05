884949fe741c0418e8a3424da352ccecd6efc1b5
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-05 22:29:26 +1100

    Wrapping up

--- objects/first/Program.cs
+++ objects/first/Program.cs
@@ -2,4 +2,10 @@
 {
     if (score >= 90)
         return "A 🌟";
+    else if (score >= 70)
+        return "B 😊";
+    else if (score >= 50)
+        return "C 😐";
+    else
+        return "F 💪 keep going!";
 }
