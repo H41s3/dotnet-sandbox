c85a2ab74b504ea814f12af1de6f6db9ec51641b
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-06 20:53:25 +1100

    Making House

--- objects/second/Program.cs
+++ objects/second/Program.cs
@@ -1,22 +1,6 @@
-﻿using namespace;
+﻿using System;
 
-public class Car
+namespace App
 {
-    public string Brand { get; set; }
-    public int Speed { get; set; }
-
-    public Car(string brand, int speed)
-    {
-        Brand = brand;
-        Speed = speed;
-    }
-
-    public void Describe() => Console.WriteLine($"{Brand} goes {Speed} mph!");
+    public class House
 }
-
-// Usage
-var car1 = new Car("Toyota", 120);
-    var car2 = new Car("Ferrari", 220);
-
-car1.Describe(); // Toyota goes 120 mph!
-car2.Describe(); // Ferrari goes 220 mph! 🏎️
