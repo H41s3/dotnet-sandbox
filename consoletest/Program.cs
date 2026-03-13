bbcdcf8aa3599df3e5fbba96886ca42f55fa9047
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-13 11:56:53 +1100

    console test modified

--- consoletest/Program.cs
+++ consoletest/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Score
+{
+    public static void Main(string[] args)
+    {
+        double score = 40;
+        if (score > 40)
+        {
+            Console.WriteLine("You passed the exam!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the exam.");
+        }
+    }
+}
