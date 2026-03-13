de094c078608591ebfd5ad3dc1a0109ad61dd0ff
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-13 13:31:53 +1100

    consoletest modified

--- consoletest/Program.cs
+++ consoletest/Program.cs
@@ -1,17 +1,6 @@
-﻿using System;
-
-public class Score
-{
-    public static void Main(string[] args)
-    {
-        double score = 40;
-        if (score > 40)
-        {
-            Console.WriteLine("You passed the exam!");
-        }
-        else
-        {
-            Console.WriteLine("You failed the exam.");
-        }
-    }
-}
+﻿int sum = 7;
+if (sum > 20)
+    Console.Write("You win ");
+else
+    Console.Write("You lose ");
+Console.WriteLine("the prize.");
