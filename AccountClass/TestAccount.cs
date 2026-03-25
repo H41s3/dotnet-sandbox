217c8f383560d46e518a34f4a0b2775e6d9ddad2
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-25 15:05:14 +1100

    TestAccount modified

--- AccountClass/TestAccount.cs
+++ AccountClass/TestAccount.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace Account
+{
+    class TestAccount
+    {
+        public static void Main()
+        {
+            Account myAccount = new Account("Alice", 1000m);
+            myAccount.Deposit(500);
+            myAccount.Withdraw(200);
+            myAccount.Print();
+        }
+    }
+}
