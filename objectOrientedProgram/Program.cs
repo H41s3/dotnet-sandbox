26573297f336eaa278519b6a17d33aca5fb3f676
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-26 23:42:06 +1100

    modified

--- objectOrientedProgram/Program.cs
+++ objectOrientedProgram/Program.cs
@@ -6,13 +6,13 @@ namespace AnimalSystem
     public class Animal
     {
         public string Name { get; set; }
-        
+
         // Constructor
         public Animal(string name)
         {
             this.Name = name;
         }
-        
+
         // Virtual method that can be overridden
         public virtual void MakeSound()
         {
@@ -24,17 +24,17 @@ namespace AnimalSystem
     public class Dog : Animal
     {
         public string Breed { get; set; }
-        
+
         // Constructor calling base class constructor
         public Dog(string name, string breed) : base(name)
         {
             this.Breed = breed;
         }
-        
+
         // Override the virtual method
         public override void MakeSound()
         {
-            Console.WriteLine("Woof! Woof!");
+            Console.WriteLine("Arf! Woof!");
         }
     }
 
@@ -46,7 +46,7 @@ namespace AnimalSystem
             Animal genericAnimal = new Animal("Generic");
             Console.WriteLine($"Animal name: {genericAnimal.Name}");
             genericAnimal.MakeSound();
-            
+
             // Create an instance of the derived class
             Dog myDog = new Dog("Peanut", "Shih Tzu");
             Console.WriteLine($"\nDog name: {myDog.Name}");
