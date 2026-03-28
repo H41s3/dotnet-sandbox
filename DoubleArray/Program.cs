5c7d2828f4449c5dc395126eb3b6c8c6ae1f25a9
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-03-28 23:11:49 +1100

    Removed some comments

--- DoubleArray/Program.cs
+++ DoubleArray/Program.cs
@@ -3,7 +3,7 @@
 //  TASK 1 — Manual Double Array
 //  Declares a double array of 10 elements, assigns each value
 //  individually, then prints each element to the console.
-// ============================================================
+
 class Program
 {
     static void Main(string[] args)
