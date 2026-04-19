f8bb37173dc3f812aa67c6a0dc8135f8077f9e0c
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-04-19 22:23:05 +1000

    Binary Search Vector file fixed

--- BinarySearch/Tester.cs
+++ BinarySearch/Tester.cs
@@ -1,213 +1,129 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Vector
 {
+
+    public class AscendingIntComparer : IComparer<int>
+    {
+        public int Compare(int A, int B) { return A - B; }
+    }
+
+    public class DescendingIntComparer : IComparer<int>
+    {
+        public int Compare(int A, int B) { return B - A; }
+    }
+
     class Tester
     {
-
-        private static bool CheckIntSequence(int[] certificate, Vector<int> vector)
-        {
-            if (certificate.Length != vector.Count) return false;
-            for (int i = 0; i < certificate.Length; i++)
-            {
-                if (certificate[i] != vector[i]) return false;
-            }
-            return true;
-        }
-
         static void Main(string[] args)
         {
-            Vector<int> vector = null;
             string result = "";
+            int problem_size = 20;
+            int[] data = new int[problem_size]; data[0] = 333;
+            Random k = new Random(1000);
+            for (int i = 1; i < problem_size; i++) data[i] = 100 + k.Next(900);
+            Vector<int> vector = null;
+            int[] temp = null; int check;
 
-            // test 1
+            // Test A
             try
             {
-                int capacity = 1;
-                Console.WriteLine("\nTest A: Create a new vector by calling 'Vector<int> vector = new Vector<int>(" + capacity + ");'");
-                vector = new Vector<int>(capacity);
-                if (vector.Capacity != capacity) throw new Exception("Vector has a wrong capacity");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "A";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new AscendingIntComparer());
+                Console.WriteLine("\nTest A: Search for key 333 in the array of integer numbers sorted via the AscendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, 333, new AscendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(333, new AscendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "A"; }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
-            // test 2
+            // Test B
             try
             {
-                Console.WriteLine("\nTest B: Add a sequence of numbers 2, 6, 8, 5, 5, 1, 8, 5, 3, 5");
-                vector.Add(2); vector.Add(6); vector.Add(8); vector.Add(5); vector.Add(5); vector.Add(1); vector.Add(8); vector.Add(5); vector.Add(3); vector.Add(5);
-                if (!CheckIntSequence(new int[] { 2, 6, 8, 5, 5, 1, 8, 5, 3, 5 }, vector)) throw new Exception("Vector stores incorrect sequence of integers");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "B";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new AscendingIntComparer());
+                Console.WriteLine("\nTest B: Search for key " + (temp[0] - 1) + " in the array of integer numbers sorted via the AscendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, temp[0] - 1, new AscendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(temp[0] - 1, new AscendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "B"; }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
-            // test 3
+            // Test C
             try
             {
-                Console.WriteLine("\nTest C: Remove number 3, 7, and then 6");
-                bool check = vector.Remove(3) && (!vector.Remove(7)) && vector.Remove(6) && (!vector.Remove(6));
-                if (!CheckIntSequence(new int[] { 2, 8, 5, 5, 1, 8, 5, 5 }, vector)) throw new Exception("Vector stores incorrect sequence of integers");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "C";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new AscendingIntComparer());
+                Console.WriteLine("\nTest C: Search for key " + (temp[problem_size - 1] + 1) + " in the array of integer numbers sorted via the AscendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, temp[problem_size - 1] + 1, new AscendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(temp[problem_size - 1] + 1, new AscendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "C"; }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
-            // test 4
+            // Test D
             try
             {
-                Console.WriteLine("\nTest D: Insert number 50 at index 6, then number 0 at index 0, then number 60 at index 'vector.Count-1', then number 70 at index 'vector.Count'");
-                vector.Insert(6, 50); vector.Insert(0, 0); vector.Insert(vector.Count - 1, 60); vector.Insert(vector.Count, 70);
-                if (!CheckIntSequence(new int[] { 0, 2, 8, 5, 5, 1, 8, 50, 5, 60, 5, 70 }, vector)) throw new Exception("Vector stores incorrect sequence of integers");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "D";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new DescendingIntComparer());
+                Console.WriteLine("\nTest D: Search for key 333 in the array of integer numbers sorted via the DescendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, 333, new DescendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(333, new DescendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "D"; }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
-            // test 5
+            // Test E
             try
             {
-                Console.WriteLine("\nTest E: Insert number -1 at index 'vector.Count+1'");
-                vector.Insert(vector.Count + 1, -1);
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine("Last operation is invalid and must throw IndexOutOfRangeException. Your solution does not match specification.");
-                result = result + "-";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new DescendingIntComparer());
+                Console.WriteLine("\nTest E: Search for key " + (temp[0] - 1) + " in the array of integer numbers sorted via the DescendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, temp[0] - 1, new DescendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(temp[0] - 1, new DescendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "E"; }
             }
-            catch (IndexOutOfRangeException exception)
-            {
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "E";
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine("Last operation is invalid and must throw IndexOutOfRangeException. Your solution does not match specification.");
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
-            // test 6
+            // Test F
             try
             {
-                Console.WriteLine("\nTest F: Remove number at index 4, then number index 0, and then number at index 'vector.Count-1'");
-                vector.RemoveAt(4); vector.RemoveAt(0); vector.RemoveAt(vector.Count - 1);
-                if (!CheckIntSequence(new int[] { 2, 8, 5, 1, 8, 50, 5, 60, 5 }, vector)) throw new Exception("Vector stores incorrect sequence of integers");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "F";
+                temp = new int[problem_size]; data.CopyTo(temp, 0);
+                Array.Sort(temp, new DescendingIntComparer());
+                Console.WriteLine("\nTest F: Search for key " + (temp[problem_size - 1] + 1) + " in the array of integer numbers sorted via the DescendingIntComparer: ");
+                vector = new Vector<int>(problem_size);
+                for (int i = 0; i < problem_size; i++) vector.Add(temp[i]);
+                Console.WriteLine("Elements in the Vector: " + vector.ToString());
+                check = Array.BinarySearch(temp, temp[problem_size - 1] + 1, new DescendingIntComparer());
+                check = check < 0 ? -1 : check;
+                if (vector.BinarySearch(temp[problem_size - 1] + 1, new DescendingIntComparer()) != check) { Console.WriteLine(" :: FAIL"); result += "-"; }
+                else { Console.WriteLine(" :: SUCCESS"); result += "F"; }
             }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
-
-            // test 7
-            try
-            {
-                Console.WriteLine("\nTest G: Remove number at index 'vector.Count'");
-                vector.RemoveAt(vector.Count);
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine("Last operation is invalid and must throw IndexOutOfRangeException. Your solution does not match specification.");
-                result = result + "-";
-            }
-            catch (IndexOutOfRangeException exception)
-            {
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "G";
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine("Last operation is invalid and must throw IndexOutOfRangeException. Your solution does not match specification.");
-                result = result + "-";
-            }
-
-            // test 8
-            try
-            {
-                Console.WriteLine("\nTest H: Run a sequence of operations: ");
-
-                Console.WriteLine("vector.Contains(1);");
-                if (vector.Contains(1)) Console.WriteLine(" :: SUCCESS");
-                else throw new Exception("1 must be in the vector");
-
-                Console.WriteLine("vector.Contains(2);");
-                if (vector.Contains(2)) Console.WriteLine(" :: SUCCESS");
-                else throw new Exception("2 must be in the vector");
-
-                Console.WriteLine("vector.Contains(4);");
-                if (!vector.Contains(4)) Console.WriteLine(" :: SUCCESS");
-                else throw new Exception("4 must not be in the vector");
-
-                Console.WriteLine("vector.Add(4); vector.Contains(4);");
-                vector.Add(4);
-                if (vector.Contains(4)) Console.WriteLine(" :: SUCCESS");
-                else throw new Exception("4 must be in the vector");
-
-                result = result + "H";
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
-
-            // test 9
-            try
-            {
-                Console.WriteLine("\nTest I: Print the content of the vector via calling vector.ToString();");
-                Console.WriteLine(vector.ToString());
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "I";
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
-
-            // test 10
-            try
-            {
-                Console.WriteLine("\nTest J: Clear the content of the vector via calling vector.Clear();");
-                vector.Clear();
-                if (!CheckIntSequence(new int[] { }, vector)) throw new Exception("Vector stores incorrect data. It must be empty.");
-                Console.WriteLine(" :: SUCCESS");
-                result = result + "J";
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(" :: FAIL");
-                Console.WriteLine(exception.ToString());
-                result = result + "-";
-            }
+            catch (Exception ex) { Console.WriteLine(" :: FAIL\n" + ex); result += "-"; }
 
             Console.WriteLine("\n\n ------------------- SUMMARY ------------------- ");
             Console.WriteLine("Tests passed: " + result);
-            Console.ReadKey();
         }
     }
 }
