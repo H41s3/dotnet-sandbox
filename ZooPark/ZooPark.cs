192b90db6731fd540b838cc1176c34b89b1f4c72
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-04-20 21:27:24 +1000

    Added ZooPark.cs file

--- ZooPark/ZooPark.cs
+++ ZooPark/ZooPark.cs
@@ -5,10 +5,10 @@ namespace ZooPark
         static void Main(string[] args)
         {
             Animal williamWolf = new Animal("William the Wolf", "Meat", "Dog Village", 50.6, 9, "Grey");
-            Animal tonyTiger   = new Animal("Tony the Tiger",   "Meat", "Cat Land",   110.0, 6, "Orange and White");
-            Animal edgarEagle  = new Animal("Edgar the Eagle",  "Fish", "Bird Mania",  20.0, 15, "Black");
+            Animal tonyTiger = new Animal("Tony the Tiger", "Meat", "Cat Land", 110.0, 6, "Orange and White");
+            Animal edgarEagle = new Animal("Edgar the Eagle", "Fish", "Bird Mania", 20.0, 15, "Black");
 
-            Console.WriteLine("=== Part 1: Animals without inheritance ===\n");
+            Console.WriteLine("Part 1: Animals without inheritance\n");
 
             williamWolf.eat();
             williamWolf.makeNoise();
