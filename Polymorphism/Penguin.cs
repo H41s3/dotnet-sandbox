cecfa0f703b9de2fbd17f6636751c0495bc337eb
Emilio Calma <calmaemiliojoseph13.8@gmail.com>
2026-04-26 22:06:17 +1000

    Penguin

---
+++ Polymorphism/Penguin.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Penguin : Bird
+{
+    public override void fly()
+    {
+        Console.WriteLine("Penguins cannot fly");
+    }
+
+    public override string ToString()
+    {
+        return "A penguin named " + base.Name;
+    }
+}
